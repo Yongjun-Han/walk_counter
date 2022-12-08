bc752928970ace831e251c8cd5779a33b3e1492e
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-08 12:54:04 +0900

    Initial Commit

---
+++ Dynamic_island/Dynamic_islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_islandApp.swift
+//  Dynamic_island
+//
+//  Created by YongJun Han on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
